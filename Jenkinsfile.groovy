51234c654cf13be8d8f0b41f21c6425c16c093b8
farrukh90 <farrukhsadykov@gmail.com>
2019-05-04 16:08:26 -0500

    created different stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,11 +3,25 @@ node{
     stage("Clone repo"){
         git 'git@github.com:farrukh90/Flaskex.git'
     }
-    stage("Build application"){
+    stage("Remove /tmp content"){
+        sh "ssh ec2-user@${ENV} sudo rm -rf /tmp/*"
+    }
+    stage("Copy filese over"){
         sh "scp -r * ec2-user@${ENV}:/tmp"
-        sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+    }
+    stage("Create Folder"){        
+        sh "ssh ec2-user@${ENV} sudo mkdir -p /flaskex"
+    }
+    stage("Copy to System"){
+        sh "ssh ec2-user@${ENV} sudo cp -r /tmp/flaskex.service /etc/systemd/system"
+    }    
+    stage("move files to /flaskex"){
+        sh "ssh ec2-user@${ENV} sudo cp -r /tmp/* /flaskex"
+    }
+    stage("Install requiremennts"){
+        sh "ssh ec2-user@${ENV} sudo pip install -r /flaskex/requirements.txt"
     }
     stage("App Run"){
-        sh "ssh ec2-user@${ENV}  python  /tmp/app.py"
+        sh "ssh ec2-user@${ENV}  systemctl start flaskex"
     }
 }
